8cae74124338b04f93fa77ed9d14b8fa3570ee9a
Nathan Anderson <nranderson14@winona.edu>
2017-10-08 15:13:48 -0500

    New image

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         //Example game
+
         try {
-           //Create a new game server
+            //Create a new game server
             Runnable gameThread = new Runnable() {
                 @Override
                 public void run() {
@@ -13,7 +15,7 @@ public class Main {
             };
             new Thread(gameThread).start();
 
-//            //Create and connect all three players
+            //Create and connect all three players
             Player p1 = new Player("p1", "127.0.0.1");
             Player p2 = new Player("p2", "127.0.0.1");
             Player p3 = new Player("p3", "127.0.0.1");
