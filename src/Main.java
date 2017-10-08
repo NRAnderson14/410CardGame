94bbc6a821914243188909a7d1384b2c4b259eec
Nathan Anderson <nranderson14@winona.edu>
2017-10-08 15:23:00 -0500

    Finished up for real this time

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,13 @@
+import java.net.UnknownHostException;
+
 public class Main {
     public static void main(String[] args) {
 
-        //Example game
-
+        //Starts the menu
         try {
-            //Create a new game server
-            Runnable gameThread = new Runnable() {
-                @Override
-                public void run() {
-                    Game testGame = new Game();
-                    testGame.startServer();
-                    testGame.playGame();
-                }
-            };
-            new Thread(gameThread).start();
-
-            //Create and connect all three players
-            Player p1 = new Player("p1", "127.0.0.1");
-            Player p2 = new Player("p2", "127.0.0.1");
-            Player p3 = new Player("p3", "127.0.0.1");
-
-
-        } catch (Exception e) {
+            MainMenu menu = new MainMenu();
+            menu.startMenu();
+        } catch (UnknownHostException e) {
             e.printStackTrace();
         }
     }
